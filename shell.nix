35d96c6c768f3b41a34c0d9e7559fe6ee72dab57
ALEZ-DEV <raidersfocus@gmail.com>
2024-07-30 18:41:31 +0200

    adding necessary dependecies to compiles mpv

--- shell.nix
+++ shell.nix
@@ -10,18 +10,29 @@ pkgs.mkShell {
     flutter
     protoc-gen-prost
     mpv-unwrapped
-  ];
-
-  buildInputs = with pkgs; [
+    libass
+    libplacebo
+    libunwind
+    shaderc
+    vulkan-loader
+    vulkan-headers
+    lcms
+    libdovi
+    libdvdnav
+    libdvdread
+    libGL
+    wayland
     openssl
     cabextract
+    ffmpeg
   ];
 
   shellHook = ''
     export OPENSSL_DIR="${pkgs.openssl.dev}"
-    export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.mpv-unwrapped.dev}/lib/pkgconfig"
+    export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.mpv-unwrapped.dev}/lib/pkgconfig:${pkgs.libass.dev}/lib/pkgconfig:${pkgs.ffmpeg.dev}/lib/pkgconfig:${pkgs.libplacebo}/lib/pkgconfig:${pkgs.libunwind.dev}/lib/pkgconfig:${pkgs.shaderc.dev}/lib/pkgconfig:${pkgs.vulkan-loader.dev}/lib/pkgconfig:${pkgs.lcms.dev}/lib/pkgconfig:${pkgs.libdovi}/lib/pkgconfig:${pkgs.libdvdnav}/lib/pkgconfig:${pkgs.libdvdread}/lib/pkgconfig"
     export OPENSSL_NO_VENDOR=1
     export OPENSSL_LIB_DIR="${pkgs.lib.getLib pkgs.openssl}/lib"
     export FLUTTER_ROOT="${pkgs.flutter}"
+    export LD_LIBRARY_PATH="${pkgs.wayland}:$LD_LIBRARY_PATH"
   '';
 }
